8ca46fc8417903f46929000d9e3999a5fdd370ee
Sandra Schuhmacher <coding@voiding-warranties.de>
2026-02-28 21:06:11 +0100

    added images for scrap items

--- second_engineer/data.lua
+++ second_engineer/data.lua
@@ -2,21 +2,21 @@
 -- Prototype definitions will be added in future milestones.
 
 local scraps = {
-  {name="scrap-red",    icon="__base__/graphics/icons/automation-science-pack.png"},
-  {name="scrap-green",  icon="__base__/graphics/icons/logistic-science-pack.png"},
-  {name="scrap-black",  icon="__base__/graphics/icons/military-science-pack.png"},
-  {name="scrap-blue",   icon="__base__/graphics/icons/chemical-science-pack.png"},
-  {name="scrap-purple", icon="__base__/graphics/icons/production-science-pack.png"},
-  {name="scrap-yellow", icon="__base__/graphics/icons/utility-science-pack.png"},
-  {name="scrap-white",  icon="__base__/graphics/icons/space-science-pack.png"},
+  {name="scrap-red",    icon="__second_engineer__/graphics/icons/scrap/automation_scrap_64x64.png"},
+  {name="scrap-green",  icon="__second_engineer__/graphics/icons/scrap/logistic_scrap_64x64.png"},
+  {name="scrap-black",  icon="__second_engineer__/graphics/icons/scrap/military_scrap_64x64.png"},
+  {name="scrap-blue",   icon="__second_engineer__/graphics/icons/scrap/chemical_scrap_64x64.png"},
+  {name="scrap-purple", icon="__second_engineer__/graphics/icons/scrap/production_scrap_64x64.png"},
+  {name="scrap-yellow", icon="__second_engineer__/graphics/icons/scrap/utility_scrap_64x64.png"},
+  {name="scrap-white",  icon="__second_engineer__/graphics/icons/scrap/space_scrap_64x64.png"},
 }
 
 if mods["space-age"] then
-  table.insert(scraps, {name="scrap-metallurgic",    icon="__space-age__/graphics/icons/metallurgic-science-pack.png"})
-  table.insert(scraps, {name="scrap-electromagnetic", icon="__space-age__/graphics/icons/electromagnetic-science-pack.png"})
-  table.insert(scraps, {name="scrap-agricultural",   icon="__space-age__/graphics/icons/agricultural-science-pack.png"})
-  table.insert(scraps, {name="scrap-cryogenic",      icon="__space-age__/graphics/icons/cryogenic-science-pack.png"})
-  table.insert(scraps, {name="scrap-promethium",     icon="__space-age__/graphics/icons/promethium-science-pack.png"})
+  table.insert(scraps, {name="scrap-metallurgic",    icon="__second_engineer__/graphics/icons/scrap/metallurgic_scrap_64x64.png"})
+  table.insert(scraps, {name="scrap-electromagnetic", icon="__second_engineer__/graphics/icons/scrap/electromagnetic_scrap_64x64.png"})
+  table.insert(scraps, {name="scrap-agricultural",   icon="__second_engineer__/graphics/icons/scrap/agricultural_scrap_64x64.png"})
+  table.insert(scraps, {name="scrap-cryogenic",      icon="__second_engineer__/graphics/icons/scrap/cryogenic_scrap_64x64.png"})
+  table.insert(scraps, {name="scrap-promethium",     icon="__second_engineer__/graphics/icons/scrap/promethium_scrap_64x64.png"})
 end
 
 local items = {}
